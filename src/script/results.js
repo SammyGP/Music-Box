7c50dc1f84bfd7c2c2d7f9d638f2f81b3b99197a
Samuel <gressboldude@gmail.com>
2017-11-13 13:30:01 +0100

    started the replace song implementation

--- src/script/results.js
+++ src/script/results.js
@@ -6,12 +6,19 @@ function getNewSong(name) {
 	var request = new Request(`http://localhost:3000/convert/${name}`);
 	fetch(request)
 	.then(function(response){
+
+		// removes the previous popup list if any
+		if(document.querySelector(".track-popup")) {
+			document.querySelector(".track-popup").remove();
+		}
 		console.log("done");
 		return response.json();
 	}).then(function(data){
 		data = JSON.parse(data);
 		data = data.items;
 		var container = document.createElement("ul");
+		container.classList.add("track-popup");
+
 		// gets the data from every song (currently 5) from the response
 		data.forEach(function(song){
 			var li = document.createElement("li");
@@ -23,6 +30,9 @@ function getNewSong(name) {
 			var p = document.createElement("p");
 			p.innerHTML = song.snippet.title;
 			li.appendChild(p);
+			var value = document.createElement("input");
+			value. = song.id.videoId;
+			value.name = "hidden";
 			container.appendChild(li);
 		})
 		document.querySelector("body").appendChild(container);
@@ -32,7 +42,7 @@ function getNewSong(name) {
 window.addEventListener("click", function(e){
 	console.log(e);
 	if(e.target.className === "change-song") {
-		var searcQuery = e.path[2].dataset.title;
-		getNewSong(searcQuery);
+		var searchQuery = e.path[2].dataset.title;
+		getNewSong(searchQuery);
 	}
 })
